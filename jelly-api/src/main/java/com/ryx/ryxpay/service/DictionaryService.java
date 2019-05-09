97a355c3c8dc86fc9ddedfd38d7e914523684719
wu_tianyu <18610639412@163.com>
2019-05-09 15:31:11 +0800

    EmployeeListener

--- jelly-api/src/main/java/com/ryx/ryxpay/service/DictionaryService.java
+++
@@ -1,9 +0,0 @@
-package com.ryx.ryxpay.service;
-
-import com.ryx.ryxpay.entity.RyxTermBrand;
-
-import java.util.List;
-
-public interface DictionaryService {
-    public List<RyxTermBrand> selectRyxTermBrandList(RyxTermBrand ryxTermBrand);
-}
